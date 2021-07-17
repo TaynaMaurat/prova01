019485ecc7d336d8a618997262c5526eb9ade38d
TaynaMaurat <86990588+TaynaMaurat@users.noreply.github.com>
2021-07-17 18:31:04 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+package App;
+
+import java.util.Scanner;
+
+import classes.Pessoa;
+import classes.Aeronave;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        Aeronave aero = new Aeronave;
+        Pessoa pess = new Pessoa(pess);
+        pess.pilotar(aero);
+
+    }
+
+    public App() {
+    }
+}
